4d25c92c4b67d206fa8310fde185765f1e978ab3
dylanbaghel <baghelabhi39@gmail.com>
2019-02-11 21:50:54 +0530

    production url

--- client/src/utils/utils.js
+++ client/src/utils/utils.js
@@ -1,5 +1,5 @@
-// export const BASE_URL = 'http://mern-blogging-app.herokuapp.com';
-export const BASE_URL = 'http://localhost:4100'
+export const BASE_URL = 'http://mern-blogging-app.herokuapp.com';
+// export const BASE_URL = 'http://localhost:4100'
 
 
 export const isEmptyObject = (obj) => {
