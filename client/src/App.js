fd09b1ba8439ee2ab55b1b7b09fac13324712c36
dylanbaghel <baghelabhi39@gmail.com>
2019-02-11 22:03:41 +0530

    added online offline capability

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
+import { Offline, Online } from 'react-detect-offline';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 import AppRouter from './router/AppRouter';
+import OfflinePage from './components/Offline';
 
 class App extends Component {
   render() {
     return (
-      <AppRouter />
+      <React.Fragment>
+        <Online>
+          <AppRouter />
+        </Online>
+        <Offline>
+          <OfflinePage />
+        </Offline>
+      </React.Fragment>
     );
   }
 }
