8e6a7dc6f4769dc21f28fb653f92d12102899b90
dylanbaghel <baghelabhi39@gmail.com>
2019-02-11 17:52:41 +0530

    some ui fix

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const HomePage = () => {
     return (
@@ -9,8 +10,8 @@ const HomePage = () => {
             <h1>Welcome To Blog App</h1>
             <small className="text-muted">Created By Abhishek Baghel V1.0.0</small>
             <div>
-                <button className="btn btn-outline-primary mr-1">Login</button>
-                <button className="btn btn-outline-success ml-1">Signup</button>
+                <Link to="/login" className="btn btn-outline-primary mr-1">Login</Link>
+                <Link to="/signup" className="btn btn-outline-success ml-1">Signup</Link>
             </div>
         </section>
     );
